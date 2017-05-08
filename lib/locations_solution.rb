f62edd54b514f2159573f96cae8d4c90baae1cdb
Fariba <massah.2@gmail.com>
2017-05-07 17:19:13 -0700

    Refactor solution

--- lib/locations_solution.rb
+++ lib/locations_solution.rb
@@ -5,41 +5,45 @@ require 'byebug'
 
 def locations_organizer(locations)
 	locations_parsed = JSON.parse(locations)
-	top_level_parents = {}
 	children = {}
 
 	locations_parsed.each do |location|
 		parent_id = location["parent_id"]
+		name = location["name"]
 		if parent_id
 			if children[parent_id]				
-				children[parent_id][ location["name"] ] = location["id"]
+				children[parent_id][name] = location["id"]
 			else 
-				children[parent_id] = { location["name"] => location["id"] }
+				children[parent_id] = { name => location["id"] }
 			end
 		else 
-			top_level_parents[location["name"]] = location["id"]
+			if children[0] 
+				children[0][name] = location["id"]
+			else
+				children[0] = { name => location["id"] }
+			end
 		end
 	end
 
+	puts children
+
 	locations_sorted = []
 	
-	top_level_parents.sort.each do |name, id|
-		locations_sorted += [name]
-		locations_sorted += child_finder(id, children)
+	children[0].sort.each do |name, id|
+		locations_sorted += [name] + next_children(id, children)
 	end
 
 	locations_sorted.join("\n")
 end
 
-def child_finder(parent_id, all_children, dashes = 0)
+def next_children(parent_id, all_children, dashes = 1)
 	return [] if all_children[parent_id].nil?
 	
-	dashes += 1
 	sorted_children = []
 
 	all_children[parent_id].sort.each do |name, id| 
 		sorted_children += ["-" * dashes + name] 
-		sorted_children += child_finder(id, all_children, dashes)
+		sorted_children += next_children(id, all_children, dashes + 1)
 	end 
 
 	sorted_children
--- spec/locations_spec.rb
+++ spec/locations_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require 'json'
-require 'locations'
+require 'locations_solution'
 
 describe 'Locations' do
 	let(:locations) { JSON.generate([{'id': 1, 'name': 'San Francisco Bay Area', 'parent_id': nil}, {'id': 2, 'name': 'San Jose', 'parent_id': 3},{'id': 3, 'name': 'South Bay', 'parent_id': 1}, {'id': 4, 'name': 'San Francisco', 'parent_id': 1}, {'id': 5, 'name': 'Manhattan', 'parent_id': 6}, {'id': 6, 'name': 'New York', 'parent_id': nil}]) }
