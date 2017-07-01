8928f42c562ecdc40082a4e05fdcf3cd0801723c
Fariba <massah.2@gmail.com>
2017-07-01 13:42:05 -0700

    Refactor list_palindrome.rb

--- ruby/solutions/list_palindrome.rb
+++ ruby/solutions/list_palindrome.rb
@@ -7,30 +7,30 @@ class ListNode
 	end
 end
 
-def isListPalindrome(l)
-    length = 0
-    node = l
-    until node.nil?
-        length += 1
-        node = node.next
-    end
+# def isListPalindrome(l)
+#     length = 0
+#     node = l
+#     until node.nil?
+#         length += 1
+#         node = node.next
+#     end
 		
-		pos1 = 0
-		pos2 = length - 1
-		until pos1 == pos2 || pos1 > pos2
+# 		pos1 = 0
+# 		pos2 = length - 1
+# 		until pos1 == pos2 || pos1 > pos2
 			
-			node1, node2 = l, l
-			pos1.times { node1 = node1.next }
-			pos2.times { node2 = node2.next }
+# 			node1, node2 = l, l
+# 			pos1.times { node1 = node1.next }
+# 			pos2.times { node2 = node2.next }
 		
-			return false if node1.value != node2.value
+# 			return false if node1.value != node2.value
 			
-			pos1 += 1
-			pos2 -= 1
-		end
+# 			pos1 += 1
+# 			pos2 -= 1
+# 		end
 
-		true 
-end
+# 		true 
+# end
 
 #Alternative solution
 
@@ -42,9 +42,7 @@ def isListPalindrome(l)
     curr1 = l
     curr2 = l2
     while !curr2.nil?
-        if curr1.value != curr2.value
-            return false
-        end
+        return false if curr1.value != curr2.value
         curr1 = curr1.next
         curr2 = curr2.next
     end
@@ -52,31 +50,31 @@ def isListPalindrome(l)
 end
 
 def find_mid(list)
-    count = 1
     slow_marker = list
     fast_marker = list.next
     while !fast_marker.nil? && !fast_marker.next.nil?
         slow_marker = slow_marker.next
-        count += 1
         fast_marker = fast_marker.next.next
     end
     slow_marker.next
 end
 
-def reverse_list(curr)
+def reverse_list(curr_node)
     prev_node = nil
-    next_node = nil
-    while !curr.nil?
-        next_node = curr.next 
-        curr.next = prev_node
-        prev_node = curr
-        curr = next_node
+    while !curr_node.nil?
+        next_node = curr_node
+        curr_node = curr_node.next
+        next_node.next = prev_node
+        prev_node = next_node
     end
     prev_node
 end
 	
 
-l = ListNode.new(0)
-l.next = ListNode.new(1)
-l.next.next = ListNode.new(0)
+l = ListNode.new(1)
+l.next = ListNode.new(2)
+l.next.next = ListNode.new(3)
+l.next.next.next = ListNode.new(4)
+l.next.next.next.next = ListNode.new(2)
+l.next.next.next.next.next = ListNode.new(1)
 puts isListPalindrome(l)
