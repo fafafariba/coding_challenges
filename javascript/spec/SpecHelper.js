6cd4f54936ff5cb0392acd9859afd0617fc1a62f
Fariba <massah.2@gmail.com>
2017-07-05 19:35:12 -0700

    Convert pairs test to ES6

--- javascript/spec/SpecHelper.js
+++ javascript/spec/SpecHelper.js
@@ -1,36 +1,18 @@
-beforeEach(function () {
-  // jasmine.addMatchers({
-  //   toBePlaying: function () {
-  //     return {
-  //       compare: function (actual, expected) {
-  //         var player = actual;
-
-  //         return {
-  //           pass: player.currentlyPlayingSong === expected && player.isPlaying
-  //         };
-  //       }
-  //     };
-  //   }
-  // });
+beforeEach( () => {
   jasmine.addMatchers({
-    toBeEquivalentToArray: function(util, customEqualityTesters) {
+    toBeEquivalentToArray: (util, customEqualityTesters) => {
       return {
-        compare: function(actual, expected) {
-          var flatten_a = actual.reduce(function(a,b) {
-            return a.concat(b);
-          }, []);
-          var flatten_e = expected.reduce(function(a, b){
-            return a.concat(b);
-          }, []);
+        compare: (actual, expected) => {
+          const flatten_a = actual.reduce((a,b) => a.concat(b), []);
+          const flatten_e = expected.reduce((a, b) => a.concat(b), []);
 
-          var equalArrays = function (arr1, arr2) {
-            for(var i = 0; i < arr1.length; i++) {
+          const equalArrays = (arr1, arr2) => {
+            for(let i = 0; i < arr1.length; i++) {
               if (arr1[i] !== arr2[i]) return false;
             }
             return true 
           }
-          // console.log(isEqual(flatten_a.sort(), flatten_e.sort())); 
-          var passed = equalArrays(flatten_a.sort(), flatten_e.sort());
+          const passed = equalArrays(flatten_a.sort(), flatten_e.sort());
           return {
             pass: passed,
             message: 'Expected ' + actual + (passed ? '' : ' not') + ' to equal ' + expected
