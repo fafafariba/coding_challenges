1f58c8d95048a02d7d04ba58bd5f8ba66aed4d8d
Fariba <massah.2@gmail.com>
2017-07-05 17:57:29 -0700

    Add pairs.js

---
+++ javascript/src/pairs.js
@@ -0,0 +1,24 @@
+
+let pairs = (arr,k) => {
+	arr.sort(compareNumbers = (a,b) => a - b);
+	i = 0;
+	j = arr.length - 1;
+	results = [];
+	while (i < j) {
+		matchI = k - arr[i]
+		matchJ = k - arr[j]
+		if (matchI == arr[j]) {
+			results.push([arr[i], arr[j]]);
+			i++;
+			j--;
+		} else if (arr[i] > matchJ ) {
+			j--;
+		} else if (arr[j] < matchI) {
+			i++;
+		} 
+	}
+	return results;
+}
+
+let a = [12, 10, 15, -1, 7, 6, 5, 4, 2, 1,]
+console.log("pairs ", pairs(a, 10));
