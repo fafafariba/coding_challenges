cb5bb53d7d65b08d1f97ef91dbab3d896df35b30
Fariba <massah.2@gmail.com>
2017-07-05 19:32:10 -0700

    Convert pairs test to ES6

--- javascript/spec/PairsSpec.js
+++ javascript/spec/PairsSpec.js
@@ -1,21 +1,21 @@
-describe("Pairs", function() {
+describe("Pairs", () => {
 
 
-  it("should return an empty array when given an empty array", function() {
+  it("should return an empty array when given an empty array", () => {
     expect(pairs([], 5)).toEqual([]);
   });
 
-  it("should return an empty array when no pairs equal k", function() {
+  it("should return an empty array when no pairs equal k", () => {
     expect(pairs([7, 1, 3, -1, 0], 5)).toEqual([]);
   });
 
-	it("should return one pair when single pair equal k", function() {
+	it("should return one pair when single pair equal k", () => {
 		let result = pairs([12, 10, 15, -1, 7, 6, 5, 4, 2, 1], 10)
 		expect(result).toBeEquivalentToArray([4, 6]);
 	});
 
-	it("should return an array of multiple pairs when multiple pairs equal k", function() {
-		var result = pairs([9, -6, 10, 5, 15, 3, -4, 1], 11);
+	it("should return an array of multiple pairs when multiple pairs equal k", () => {
+		let result = pairs([9, -6, 10, 5, 15, 3, -4, 1], 11);
 		expect(result).toEqual([[-4, 15], [1, 10]]);
 	});
 });
